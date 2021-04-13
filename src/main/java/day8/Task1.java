fd12538a802e702e4cf8c983d65f167398fee99f
Maksim Nikonov <center7@mail.ru>
2021-04-13 13:31:13 +0300

    complete day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,41 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        int arraySize = 20001;
+
+        long start = System.currentTimeMillis();
+
+        String[] strArr = new String[arraySize];
+        String s = "";
+
+        for (int i = 0; i < strArr.length; i++) {
+            strArr[i] = Integer.toString(i) + " ";
+            s = s + strArr[i];
+        }
+
+        System.out.println(s);
+
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+
+        System.out.println(timeConsumedMillis);
+
+        long start1 = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < arraySize; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+
+        long finish1 = System.currentTimeMillis();
+        long timeConsumedMillis1 = finish1 - start1;
+
+        System.out.println(timeConsumedMillis1);
+
+
+
+
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -3,5 +3,9 @@ package day8;
 public class Task2 {
     public static void main(String[] args) {
 
+        Airplane plane1 = new Airplane("Mig", 2020, 12000, 25000);
+
+        System.out.println(plane1);
+
     }
 }
